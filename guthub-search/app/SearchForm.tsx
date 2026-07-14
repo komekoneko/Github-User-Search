6148fd591fba1ffa45574822368c99ca0f0bde83
komekoneko <kai.s20050701@gmail.com>
2026-07-14 23:05:26 +0900

    add Enter や 空文字検知

--- guthub-search/app/SearchForm.tsx
+++ guthub-search/app/SearchForm.tsx
@@ -1,23 +1,35 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import { useRouter } from "next/navigation"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default function SearchForm(){
-    const [ input, setInput ] = useState("");
-    const router = useRouter()
+export default function SearchForm() {
+  const [input, setInput] = useState("");
+  const router = useRouter();
 
-    const searchFn = () => {
-        router.push(`/users/${input}`)
-        setInput("")
+  const searchFn = () => {
+    const newInput = input.trim();
+    if (newInput === "") {
+      return alert("ユーザー名を入力してください");
     }
+    router.push(`/users/${newInput}`);
+    setInput("");
+  };
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if(e.key === "Enter"){
+        searchFn()
+    }
+  }
 
-    return(
-        <>
-        <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-        <button onClick={() => searchFn()}>検索</button>
-        </>
-        
-
-    )
+  return (
+    <>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => handleKeyDown(e)}
+      />
+      <button onClick={() => searchFn()}>検索</button>
+    </>
+  );
 }
