28d3da7cf4e439dfa48cfc9d3dc16b1f3d960f18
komekoneko <kai.s20050701@gmail.com>
2026-07-14 22:31:34 +0900

    add repository(ユーザー情報の拡張)

--- guthub-search/app/users/[username]/page.tsx
+++ guthub-search/app/users/[username]/page.tsx
@@ -5,6 +5,14 @@ type GitHubUser = {
     public_repos: number
 }
 
+type Repo = {
+    id: number,
+    name: string,
+    description: string | null,
+    html_url: string,
+    stargazers_count: number
+}
+
 export default async function UserPage({
     params,
 }: {
@@ -19,6 +27,10 @@ export default async function UserPage({
 
     const user: GitHubUser = await res.json();
 
+    const repoRes = await fetch(`https://api.github.com/users/${username}/repos`)
+
+    const repoUser: Repo[] = await repoRes.json();
+
 
     return(
         <div>
@@ -26,6 +38,17 @@ export default async function UserPage({
             <img src={user.avatar_url} width={100} height={100} />
             <p>{user.login}</p>
             <p>公開リポジトリ: {user.public_repos}</p>
+            <ul>
+                {repoUser.map((repo) => {
+                    return(
+                        <li key={repo.id}>
+                            <a href={repo.html_url}>{repo.name}</a>
+                            <p>{repo.description ?? "説明なし"}</p>
+                            <span>⭐️{repo.stargazers_count}</span>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
         
     )
